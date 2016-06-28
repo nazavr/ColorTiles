8659a745d4b9811b6a207de3383a87a57cca6fa2
nazavr <nazavr@gmail.com>
2016-06-28 16:51:01 +0000

    add new file

---
+++ index.js
@@ -0,0 +1,52 @@
+function ColorTilesGame(props) {
+    var lastSelectedElement, 
+        tiles = document.querySelectorAll(props.componentSelector + ' li>div');
+    document.getElementById('square').addEventListener('click', function(e) {
+        if (e.target.nodeName === 'DIV') {
+            tileClick(e.target);
+        }
+    }, false);
+
+    function tileClick(tile) {
+
+        hightlightTile(tile);
+
+        if (!lastSelectedElement) {
+            lastSelectedElement = tile;
+            return false;
+        }
+
+        if (checkIsTheSame(tile)) {
+            setTimeout(function () {
+                //lastSelectedElement.style.opacity = "0";
+                //tile.style.opacity = "0";
+                document.querySelector("h2").innerHTML = "You win!!! Elements have the same color! Try again!";
+            }, 1500);
+            setTimeout(tryAgain, 4000);
+        } else {
+            setTimeout(function () {
+                //lastSelectedElement.style.opacity = "0";
+                //tile.style.opacity = "0";
+                document.querySelector("h2").innerHTML = "Sorry! Different tiles! Try again!";
+            }, 1500);
+            setTimeout(tryAgain, 4000);
+        }
+    }
+
+    function checkIsTheSame(tile) {
+        return lastSelectedElement.dataset.colorName === tile.dataset.colorName;
+    }
+
+    function hightlightTile(tile) {
+        tile.style.backgroundColor = tile.dataset.colorName;
+    }
+
+    function tryAgain() {
+        for (var i = 0; i < tiles.length; i++) {
+            tiles[i].style.backgroundColor = '';
+            document.querySelector("h2").innerHTML = "Please select two random tiles";
+        }
+
+        lastSelectedElement = null;
+    }
+}
